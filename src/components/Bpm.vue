a3f6e1853aa9b4285068985e2eeeb8eb28842e5e
Eric Araújo <eric@safetysystemtechnology.com.br>
2017-11-26 11:00:26 -0300

    Beat calc

--- src/components/Bpm.vue
+++ src/components/Bpm.vue
@@ -8,18 +8,21 @@ import vueBus from '../services/Bus'
 export default {
   name: 'bpm',
   data: () => ({
-    beat: 60
+    beat: 60,
+    time: null
   }),
   created () {
     vueBus.$on('decreaseValue', (value) => {
       this.beat = this.beat - value
+      this.time = vueBus.beatTime(this.beat)
     })
     vueBus.$on('increaseValue', (value) => {
       this.beat = this.beat + value
+      this.time = vueBus.beatTime(this.beat)
     })
     vueBus.$on('beatValueFromSlider', (value) => {
-      console.log(value)
       this.beat = value
+      this.time = vueBus.beatTime(this.beat)
     })
   }
 }
--- src/services/Bus.js
+++ src/services/Bus.js
@@ -1,3 +1,10 @@
 import Vue from 'vue'
-const Bus = new Vue()
+const Bus = new Vue({
+  methods: {
+    beatTime (value) {
+      const beat = (60 / value) * 1000
+      return Math.floor(beat)
+    }
+  }
+})
 export default Bus
