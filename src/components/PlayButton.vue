462efe80d61329fb154d74012377782ab76095a8
Eric Araújo <eric@safetysystemtechnology.com.br>
2017-11-26 21:49:14 -0300

    Audio added

--- src/components/PlayButton.vue
+++ src/components/PlayButton.vue
@@ -1,16 +1,29 @@
 <template>
   <el-row id="playButtonWrapper">
     <el-col :span="24">
-      <img id="playButton" src="../assets/play-button.svg" alt="">
+      <img id="playButton" src="../assets/play-button.svg" alt="Play Button" @click="changeRythm" :class="{'rythm-bass': rythmBass}">
     </el-col>
   </el-row>
 </template>
 <script>
+import Rythm from 'rythm.js'
 export default {
   name: 'playButton',
   data: () => ({
-
-  })
+    rythmBass: false
+  }),
+  methods: {
+    changeRythm () {
+      const myAudioCtx = new AudioContext()
+      myAudioCtx.close().then(() => {
+        this.rythmBass = !this.rythmBass
+        const rythm = new Rythm()
+        rythm.stop()
+        rythm.setMusic('../../static/tick.mp3')
+        rythm.start()
+      })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
